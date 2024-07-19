0f3a788d38040991442572e89559c70f714f53fe
Adauto Marques <adautomarques937@gmail.com>
2024-07-19 10:30:05 -0300

    Obtendo usuarios do banco de dados

--- public/index.php
+++ public/index.php
@@ -4,5 +4,8 @@ require_once(dirname(__FILE__) ."/../src/config/config.php");
 require_once(dirname(__FILE__) ."/../src/models/User.php");
 
 $user = new User(['name' => 'Lucas', 'email'=> 'lucas@gmail.com',]);
-print_r($user);
+print_r(User::get(['id' => 1], 'name, email'));
+
+//echo User::getSelect(['name'=> 'Chaves']);
+
 
